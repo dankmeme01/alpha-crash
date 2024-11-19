fcf16b3a411001020aecfa7b0cf70bb70ccc12d3
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2024-11-20 00:51:19 +0100

    more minimal

--- src/ui/hooks/MenuLayer.h
+++ src/ui/hooks/MenuLayer.h
@@ -30,20 +30,6 @@ class $modify(MyMenuLayer, MenuLayer) {
 
 		FMODAudioEngine::sharedEngine()->playEffect("click.ogg"_spr);
 
-        float scale = CCDirector::sharedDirector()->getContentScaleFactor()/4;
-
-		Utils::getNodeSafe(this, "main-menu")->setVisible(false);
-		Utils::getNodeSafe(this, "profile-menu")->setVisible(false);
-		Utils::getNodeSafe(this, "player-username")->setVisible(false);
-		Utils::getNodeSafe(this, "social-media-menu")->setVisible(false);
-		Utils::getNodeSafe(this, "close-menu")->setVisible(false);
-		Utils::getNodeSafe(this, "bottom-menu")->setVisible(false);
-		Utils::getNodeSafe(this, "more-games-menu")->setVisible(false);
-		Utils::getNodeSafe(this, "main-menu-bg")->setVisible(false);
-		Utils::getNodeSafe(this, "main-menu-bg")->setScale(0);
-		Utils::getNodeSafe(this, "right-side-menu")->setVisible(false);
-		Utils::getNodeSafe(this, "main-title")->setVisible(false);
-
 		CCMenu* menu = CCMenu::create();
 
 		MCButton* playButton = MCButton::create("Singleplayer", 49.1f, this, menu_selector(MenuLayer::onPlay));
@@ -58,22 +44,14 @@ class $modify(MyMenuLayer, MenuLayer) {
 
 		setPositions(winSize);
 
-		this->schedule(schedule_selector(MyMenuLayer::updateStuff), 0.1f);
-
 		return true;
 	}
 
-	void updateStuff(float) {
-	}
-
 	void onPlay(CCObject* s)  {
-
 		MenuLayer::onPlay(s);
 
 		CCNode* menu = this->getChildByID("minecraft-menu"_spr);;
 		MCButton* playButton = typeinfo_cast<MCButton*>(menu->getChildByID("play-button"_spr));
-
-		log::debug("rc = {}, rcc = {}", playButton->retainCount(), playButton->child->retainCount());
 	}
 
 	void setPositions(CCSize winSize){
