596f145f27c61f6c8bc709a68357ede6bf50dabd
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2024-11-20 01:29:58 +0100

    ff

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,34 @@
 #include <Geode/Geode.hpp>
-#include "ui/hooks/MenuLayer.h"
-#include "ui/hooks/CCMenuItemSpriteExtra.h"
-#include "ui/hooks/CCMenuItemToggler.h"
+#include <Geode/modify/MenuLayer.hpp>
+#include <Geode/modify/CCLabelBMFont.hpp>
+
+using namespace geode::prelude;
+
+class MCLabel : public CCLabelBMFont {
+public:
+    static MCLabel* create(std::string text, std::string font) {
+        MCLabel *ret = new MCLabel();
+        if (ret->init(text, font)) {
+            return static_cast<MCLabel*>(ret->autorelease());
+        } else {
+            delete ret;
+            return nullptr;
+        }
+    }
+
+    bool init(std::string text, std::string font) {
+        return CCLabelBMFont::initWithString(text.c_str(), font.c_str());
+    }
+};
+
+
+class $modify(MyMenuLayer, MenuLayer) {
+    bool init() {
+        if (!MenuLayer::init()) return false;
+
+		FMODAudioEngine::sharedEngine()->playEffect("click.ogg"_spr);
+		MCLabel* label2 = MCLabel::create("Singleplayer", "minecraft.fnt"_spr);
+
+        return true;
+    }
+};
--- src/ui/hooks/CCMenuItemSpriteExtra.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <Geode/Geode.hpp>
-#include <Geode/modify/CCMenuItemSpriteExtra.hpp>
-
-class $modify(MyCCMenuItemSpriteExtra, CCMenuItemSpriteExtra){
-};
